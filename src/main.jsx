64466e2ec03382d3acfe105ca973e8b376c1d7d6
rstubryan <restubumirr@gmail.com>
2024-06-25 06:32:49 +0700

    feat(+): add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TodoPages from "./pages/TodoPages.jsx";
+import ListTodoPages from "@/pages/ListTodoPages.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/todo",
+    element: <TodoPages />,
+  },
+  {
+    path: "/",
+    element: <ListTodoPages />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
